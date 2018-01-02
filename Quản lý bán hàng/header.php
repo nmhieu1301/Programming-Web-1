8bbfb944a996d48684aa639332bc9c9203ecbd64
nmhieu1301 <nmhieu1301@users.noreply.github.com>
2018-01-02 21:45:49 +0700

    Minh Hiếu
    index, header, footer

---
+++ Quản lý bán hàng/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Trang chủ</title>
+		<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+	</head>
+<body>
+	<div class="container-fluid"?
+		<nav class="navbar navbar-inverse">
+		  <div class="container-fluid">
+			<div class="navbar-header">
+			  <a class="navbar-brand" href="#">WebSiteName</a>
+			</div>
+			<li class="nav navbar-nav">
+			  <li class="active"><a href="#">Home</a></li>
+			  <ul><a href="#">Page 1</a></ul>
+			  <li><a href="#">Page 2</a></li>
+			  <li><a href="#">Page 3</a></li>
+			</li>
+		  </div>
+		</nav>
+	</div>
+</body>
+</html>
+
